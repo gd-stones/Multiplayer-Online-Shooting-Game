64a1b3b0cf442a513ca86167ecaea4729d0eb9e8
gd-stones <ngducviet2105@gmail.com>
2024-02-14 11:46:36 +0700

    Player Camera Movement

--- Assets/Game/Scripts/PlayerControls/InputManager.cs
+++ Assets/Game/Scripts/PlayerControls/InputManager.cs
@@ -5,8 +5,11 @@ public class InputManager : MonoBehaviour
     private PlayerControls playerControls;
     private AnimatorManager animatorManager;
     public Vector2 movementInput;
+    public Vector2 cameraMovementInput;
     public float verticalInput;
     public float horizontalInput;
+    public float cameraInputX;
+    public float cameraInputY;
     private float movementAmount;
 
     private void Awake()
@@ -20,6 +23,7 @@ public class InputManager : MonoBehaviour
         {
             playerControls = new PlayerControls();
             playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            playerControls.PlayerMovement.CameraMovement.performed += i => cameraMovementInput = i.ReadValue<Vector2>();
         }
 
         playerControls.Enable();
@@ -39,6 +43,10 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+
+        cameraInputX = cameraMovementInput.x;
+        cameraInputY = cameraMovementInput.y;
+
         movementAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
         animatorManager.ChangeAnimatorValues(0, movementAmount);
     }
--- Assets/Game/Scripts/PlayerMovement/CameraManager.cs
+++ Assets/Game/Scripts/PlayerMovement/CameraManager.cs
@@ -2,18 +2,47 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    InputManager inputManager;
     public Transform playerTransform;
+    public Transform cameraPivot;
     private Vector3 cameraFollowVelocity = Vector3.zero;
     public float cameraFollowSpeed = 0.3f;
+    public float camLookSpeed = 2f; 
+    public float camPivotSpeed = 2f; 
+    public float lookAngle;
+    public float pivotAngle;
 
     private void Awake()
     {
+        inputManager = FindObjectOfType<InputManager>();
         playerTransform = FindObjectOfType<PlayerManager>().transform;
     }
 
-    public void FollowTarget()
+    public void HandleAllCameraMovement()
+    {
+        FollowTarget();
+        RotateCamera();
+    }
+
+    private void FollowTarget()
     {
         Vector3 targetPosition = Vector3.SmoothDamp(transform.position, playerTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
         transform.position = targetPosition;
+    }
+
+    private void RotateCamera()
+    {
+        lookAngle = lookAngle + (inputManager.cameraInputX * camLookSpeed);
+        pivotAngle = pivotAngle + (inputManager.cameraInputY * camPivotSpeed);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivot.localRotation = targetRotation;
     }
 }
