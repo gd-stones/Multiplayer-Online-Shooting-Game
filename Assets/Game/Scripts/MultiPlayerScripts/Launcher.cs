8c1656fbb9b211244d88632782e0ce9028844beb
gd-stones <ngducviet2105@gmail.com>
2024-03-05 22:32:31 +0700

    connect to master photon - done lesson #22

---
+++ Assets/Game/Scripts/MultiPlayerScripts/Launcher.cs
@@ -0,0 +1,22 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        Debug.Log("connecting to master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connect to master");
+        OnJoinedLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+    }
+}
