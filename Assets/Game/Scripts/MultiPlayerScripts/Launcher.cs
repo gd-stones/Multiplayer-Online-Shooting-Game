0b27f5e02819e639884e5a997cd5c6301c53b126
gd-stones <ngducviet2105@gmail.com>
2024-03-06 22:14:23 +0700

    create room and join room - done lesson #24

--- Assets/Game/Scripts/MultiPlayerScripts/Launcher.cs
+++ Assets/Game/Scripts/MultiPlayerScripts/Launcher.cs
@@ -1,8 +1,13 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private InputField roomNameInputField;
+    [SerializeField] private Text roomNameText;
+    [SerializeField] private Text errorText;
+
     private void Start()
     {
         Debug.Log("connecting to master");
@@ -19,5 +24,37 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("TitleMenu");
         Debug.Log("joined lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (!string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            PhotonNetwork.CreateRoom(roomNameInputField.text);
+            MenuManager.Instance.OpenMenu("LoadingMenu");
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("RoomMenu");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string errorMessage)
+    {
+        errorText.text = "Room Creation Failed: " + errorMessage;
+        MenuManager.Instance.OpenMenu("ErrorMenu");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("TitleMenu");
     }
 }
